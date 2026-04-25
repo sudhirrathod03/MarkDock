b58501786658325a9768fab67c13a885d433d33e
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-25 21:33:34 +0530

    Refactor GET function to handle authentication code

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,7 +5,6 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/dashboard'
-
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
